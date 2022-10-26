6faa69c4b4bd42194c91091ceb802bdd1c405ddc
ConnorB <cb582176@wne.edu>
2022-10-26 18:08:04 -0400

    Add center white square and title rectangle containing 'tic tac toe'

--- tictactoe.py
+++ tictactoe.py
@@ -3,14 +3,30 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_width = 500
-screen_height = 500
+screen_width = 600
+screen_height = 600
+screen_color = (0,150,255)
 
 screen = pygame.display.set_mode((screen_width, screen_height))
+screen.fill(screen_color)
+pygame.display.set_caption('Tic Tac Toe')
+
+main_rec_color = (255,255,255)
+pygame.draw.rect(screen, main_rec_color, pygame.Rect(100,100,400,400))
+
+##title_rec_color = (255,255,255)
+##pygame.draw.rect(screen, title_rec_color, pygame.Rect(100,10,400,50))
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+title = font.render('Tic Tac Toe', True, (0,0,0), (255,255,255))
+title_rect = title.get_rect()
+title_rect.center = (300,50)
+
+screen.blit(title, title_rect)
 
 game = True
 while game:
-
+    
     for event in pygame.event.get():
         if event.type == pygame.quit:
             game = False
