08813954d8199098544c5945270674451b6e9b3b
SirCurMonkee <joshua.ryan@wne.edu>
2022-10-26 19:57:46 -0400

    Added notes for findBestMove() usage

--- tictactoe.py
+++ tictactoe.py
@@ -9,7 +9,11 @@ screen_height = 500
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-# 
+# '_' = available move
+# 'X' = player
+# 'O' = opponent
+# to get cpu move, call cpu.findBestMove(board)
+# will return bestMove(row, column)
 board = [
     [ '_', '_', '_' ],
     [ '_', '_', '_' ],
